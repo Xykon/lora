59ce6cd2487dd97a53b27aea1cad2eb08de84752
Christian Ehlers <christian@ehlers.me.uk>
2016-12-06 00:43:40 +0000

    Add sender & receiver for RN2483 using python-loranode

---
+++ rn2483/p2p_recv.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import struct
+from loranode.rpyutils import printd, Level, Color, clr, set_debug_level
+from threading import Thread, Lock
+from loranode import RN2483Controller
+from time import sleep
+
+class ReceiverThread(Thread):
+    def __init__(self, mutex):
+        Thread.__init__(self)
+        self.setDaemon(True)
+        self.lc = RN2483Controller("/dev/ttyACM0")
+        self.lc.set_sf("7")
+        self.lc.set_pwr("15")
+        self.lc.get_bt()
+        self.lc.set_bt("none")
+        #self.lc.set_freq("868000000")
+        print("LoRa frequency: ",self.lc.get_freq())
+        self.mutex = mutex
+        self.counter = 0
+
+    def run(self):
+        while True:
+            r = self.lc.recv_p2p()
+
+            self.mutex.acquire() # Do not let prints occur simultaneously
+            printd(clr(Color.BLUE, "RECV[" + str(self.counter) + "]: " + r), Level.INFO)
+            self.counter += 1
+            self.mutex.release()
+
+# LoRaController peer-to-peer test, assuming two devices at /dev/ttyUSB0 and
+# /dev/ttyUSB1
+if __name__ == "__main__":
+    set_debug_level(Level.DEBUG)
+
+    m = Lock()
+
+    r = ReceiverThread(m)
+    r.start()
+
+    r.join()
---
+++ rn2483/p2p_send.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import struct
+from loranode.rpyutils import printd, Level, Color, clr, set_debug_level
+from threading import Thread, Lock
+from loranode import RN2483Controller
+from time import sleep
+
+class TransmitterThread(Thread):
+    def __init__(self, mutex):
+        Thread.__init__(self)
+        self.setDaemon(True)
+        self.lc = RN2483Controller("/dev/ttyACM0")
+        self.lc.set_sf("7")
+        self.lc.set_pwr("15")
+        self.lc.get_bt()
+#        self.lc.set_bt("none")
+#        self.lc.set_freq("868000000")
+        print("LoRa frequency: ",self.lc.get_freq())
+        self.mutex = mutex
+        self.counter = 4278190080
+#        self.counter = 0
+
+    def run(self):
+        while True:
+            data = struct.pack(">I", self.counter).hex()
+            self.counter += 1
+
+            self.mutex.acquire()
+            printd(clr(Color.GREEN, "SEND: " + data), Level.INFO)
+            self.mutex.release()
+
+            r = self.lc.send_p2p(data)
+            sleep(3)
+
+# LoRaController peer-to-peer test, assuming two devices at /dev/ttyUSB0 and
+# /dev/ttyUSB1
+if __name__ == "__main__":
+    set_debug_level(Level.DEBUG)
+
+    m = Lock()
+
+    t = TransmitterThread(m)
+    t.start()
+
+    t.join()
